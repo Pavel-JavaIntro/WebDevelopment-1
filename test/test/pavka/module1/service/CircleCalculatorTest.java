bae14473ee06887339933ffb0c95d85babac4626
Pavel-JavaIntro <62737771+Pavel-JavaIntro@users.noreply.github.com>
2020-06-10 06:59:17 +0300

    Update CircleCalculatorTest.java

--- test/test/pavka/module1/service/CircleCalculatorTest.java
+++ test/test/pavka/module1/service/CircleCalculatorTest.java
@@ -5,15 +5,22 @@ import by.pavka.module1.service.CircleCalculator;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
 public class CircleCalculatorTest {
   private CircleCalculator circleCalculator = new CircleCalculator();
 
-  @Test
-  public void testFindLengthAndArea() throws NegativeRadiusException {
+@Test
+  public void testFindLengthAndArea() {
     double radius = 2;
     double[] expected = {Math.PI * 4, Math.PI * 4};
-    assertEquals(circleCalculator.findLengthAndArea(radius), expected);
+    double[] actual = null;
+    try {
+      actual = circleCalculator.findLengthAndArea(radius);
+    } catch (NegativeRadiusException e) {
+      fail();
+    }
+    assertEquals(actual, expected);
   }
 
   @Test (expectedExceptions = NegativeRadiusException.class)
